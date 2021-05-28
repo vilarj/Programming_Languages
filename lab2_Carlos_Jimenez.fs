f0bf81e214e5650ca47b52e18a7d30b0397f3fa0
Vilarj <lamaravillasan@outlook.com>
2021-05-28 11:47:46 -0400

    Need to fininsh part 4 and convert float into big int (problem 2)

--- lab2_Carlos_Jimenez.fs
+++ lab2_Carlos_Jimenez.fs
@@ -14,3 +14,19 @@ let nf n k = !n / (!k * !(n - k))
 printfn "\nProblem 2\nC(10, 5) = %A"  <| ``nf`` 10. 5.
 
 // Problem 3
+let rec vecadd a b =
+    match a, b with
+    | [], [] -> []
+    | a::atail, b::btail -> (a + b) :: (vecadd atail btail)
+    
+printfn "\nProblem 3\n%A(i) + %A(k) = %A(i+k)" [1;2;3] [4;5;6] (vecadd [1;2;3] [4;5;6])
+
+// Problem 4
+let m1 = [[1;4],[2;5],[3;6]]
+let m2 = [[7;8;9],[1;2;3]]
+
+let rec matadd a b =
+    match a, b with
+    | [], [] -> []
+    | a::atail, b::btail -> (a*b) :: vecadd atail btail
+    | _ ->
