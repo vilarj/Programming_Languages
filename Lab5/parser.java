ca1d03c2dac190ce0cd56f8b75cf19f5073d4337
Carlos Jimenez <lamaravillasan@outlook.com>
2021-07-12 22:22:30 -0700

    Some more work...

--- Lab5/parser.java
+++ Lab5/parser.java
@@ -1,29 +1,54 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Parser {
-    private static Scanner input;
-    public static String toEvaluate = "8 + 2 * (5-2)";
-    public static char[] toParse;
-    private static int lexLen;
-    private static char nextChar;
-    private static char lexeme[];
-    
-    private static final int LETTER = 0;
-    private static final int DIGIT = 1;
-    private static final int UNKNOWN = 99;
+    static final int MAX_LEXEME_LENGTH = 100;
+    static final int MAX_TOKEN_LENGTH = 100;
+    static int charClass;
+    static char[] lexeme = new char[MAX_LEXEME_LENGTH];
+    static char nextChar;
+    static int lexLen;
+    static int nextToken;
+    static FileReader file;
+    static BufferedReader reader;
+    static final int LETTER = 0;
+    static final int DIGIT = 1;
+    static final int UNKNOWN = 99;
+    static String[] token_dict = new String[MAX_TOKEN_LENGTH];
+    static final int INT_LIT = 10;
+    static final int IDENT = 11;
+    static final int ASSIGN_OP = 20;
+    static final int ADD_OP = 21;
+    static final int SUB_OP = 22;
+    static final int MULT_OP = 23;
+    static final int DIV_OP = 24;
+    static final int LEFT_PAREN = 25;
+    static final int RIGHT_PAREN = 26;
+    static final int END_OF_FILE = 98;
 
-    private static final int INT_LIT = 10;
-    private static final int IDENT = 11;
-    private static final int ASSIGN_OP = 20;
-    private static final int ADD_OP = 21;
-    private static final int SUB_OP = 22;
-    private static final int MULT_OP = 23;
-    private static final int DIV_OP = 24;
+    public static void read_file(FileReader file) {
+        reader = new BufferedReader(file);
 
-    private static final int LEFT_PAREN = 25;
-    private static final int RIGHT_PAREN = 26;
+        if (file == null){
+            System.out.println("ERROR - cannot open codes.txt");
+        }else{
+            try {
+                while(reader.ready()){
+                    getChar();
+                    do
+                    {
+                        lex();
+                        assign();
+                    }while(nextToken != END_OF_FILE);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s\t+\t", token_dict[nextToken]);
+        }
+    }
 
-    private static int lookup(char ch){
+    static int lookup(char ch){
+
         switch (ch) {
             case '(':
                 addChar();
@@ -66,42 +91,118 @@ public class Parser {
                 lexeme[2] = 'F';
                 lexeme[3] = 0;
                 lexLen = 4;
-                nextToken = 'END_OF_FILE';
+                nextToken = END_OF_FILE;
                 break;
         }
         return nextToken;
     }
 
-    public static void addChar(){
-        if (lexLen <= 98) {
+    static void addChar(){
+
+        if (lexLen <= 98)
+        {
             lexeme[lexLen++] = nextChar;
             lexeme[lexLen] = 0;
-        } 
-        else {System.out.println("Error - lexeme is too long \n");}
+        } else
+            System.out.println("Error - lexeme is too long \n");
     }
 
-    static boolean isAlpha(char c){
+    static void getChar(){
+        char c = 0;
+
+        try {c = (char)reader.read();}
+        catch (IOException e) {e.printStackTrace();}
+
+        nextChar = c;
+
+        if((int)nextChar != 0){
+            if(isalpha(nextChar)){
+                charClass = LETTER;
+            }else if(isdigit(nextChar)){
+                charClass = DIGIT;
+            }else{ charClass = UNKNOWN;}
+        }else{
+            charClass = END_OF_FILE;
+        }
+    }
+
+    static void getNonBlank(){
+        while(isspace(nextChar)){
+            getChar();
+        }
+    }
+
+    public static int lex(){
+        lexLen = 0;
+        lexeme = new char[100];
+        getNonBlank();
+        switch (charClass){
+
+            case LETTER:
+                addChar();
+                getChar();
+                while(charClass == LETTER || charClass == DIGIT){
+                    addChar();
+                    getChar();
+                }
+                nextToken = IDENT;
+                break;
+
+            case DIGIT:
+                addChar();
+                getChar();
+                while(charClass == DIGIT){
+                    addChar();
+                    getChar();
+                }
+                nextToken = INT_LIT;
+                break;
+
+            case UNKNOWN:
+                lookup(nextChar);
+                getChar();
+                break;
+
+            case END_OF_FILE:
+                nextToken = END_OF_FILE;;
+                lexeme[0] = 'E';
+                lexeme[1] = 'O';
+                lexeme[2] = 'F';
+                lexeme[3] = 0;
+                lexLen = 4;
+                break;
+        }
+        if (nextToken != END_OF_FILE) {
+            String s = new String(lexeme);
+            s = s.substring(0,lexLen);
+            System.out.printf("%s\t+\t%s\n", token_dict[nextToken], s);}
+        return nextToken;
+    }
+
+    static boolean isalpha(char c){
+
         int ascii = (int) c;
-
-        if((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123)){return true;}
-        else {return false;}
+        if((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123)){
+            return true;
+        }else {return false;}
     }
 
-    static boolean isDigit(char c){
+    static boolean isdigit(char c){
+
         int ascii = (int) c;
-
-        if(ascii > 47 && ascii < 58){return true;}
-        else {return false;}
+        if(ascii > 47 && ascii < 58){
+            return true;
+        }else {return false;}
     }
 
-    static boolean isSpace(char c){
+    static boolean isspace(char c){
+
         int ascii = (int) c;
-
-        if(ascii == 32){return true;}
-        else {return false;}
+        if(ascii == 32){
+            return true;
+        }else {return false;}
     }
 
-        //Assign Function
     // <assign> -> id = <expr>
     static void assign() throws IOException {
         if (nextToken == IDENT)
@@ -118,7 +219,6 @@ public class Parser {
      <expr> -> <term> {(+ | -) <term>}
      */
     static void expr() throws IOException {
-        /* Parse the first term */
         term();
 	/* As long as the next token is + or -, get
 	 the next token and parse the next term */
@@ -126,29 +226,22 @@ public class Parser {
             lex();
             term();
         }
-    } /* End of function expr */
+    }
 
     //Parses strings in the language generated by the rule:
     //<term> -> <factor> {(* | /) <factor>)}
-
     static void term() throws IOException {
-        /* Parse the first factor */
         factor();
 	/* As long as the next token is * or /, get the
 	 next token and parse the next factor */
-        while (nextToken == MULT_OP || nextToken == DIV_OP)
-        {
+        while (nextToken == MULT_OP || nextToken == DIV_OP) {
             lex();
             factor();
         }
-    } /* End of function term */
+    }
 
     static void factor() throws IOException {
-        /* Determine which RHS */
-        if (nextToken == IDENT || nextToken == INT_LIT)
-            /* Get the next token */
-            lex();
-
+        if (nextToken == IDENT || nextToken == INT_LIT) {lex();}
 	/* If the RHS is ( <expr>), call lex to pass over the
 	 left parenthesis, call expr, and check for the right
 	 parenthesis */
@@ -161,24 +254,26 @@ public class Parser {
                     lex();
                 else
                     System.out.println("ERROR: Does not have RIGHT_PAREN");
-            } /* End of if (nextToken == ... */
-	/* It was not an id, an integer literal, or a left
-	 parenthesis */
+            }
             else {
                 System.exit(0);
             }
-        } /* End of else */
-    } 
-    
-    public static void readExpression() {
-        for (int pos = 0; pos < toEvaluate.lenght(); pos++) {
-            toParse[pos] = toEvaluate[pos];
         }
     }
 
-    public static void main(String[] args) {
-        readExpression(); // filling up the array of characters with the expression to parse
+    public static void main(String[] args) throws FileNotFoundException {
+        token_dict[INT_LIT] = "INT_LIT\t";
+        token_dict[IDENT] = "IDENT\t";
+        token_dict[ASSIGN_OP] = "ASSIGN_OP";
+        token_dict[ADD_OP] = "ADD_OP\t";
+        token_dict[SUB_OP] = "SUB_OP\t";
+        token_dict[MULT_OP] = "MULT_OP\t";
+        token_dict[DIV_OP] = "DIV_OP\t";
+        token_dict[LEFT_PAREN] = "LEFT_PAREN";
+        token_dict[RIGHT_PAREN] = "RIGHT_PAREN";
+        token_dict[END_OF_FILE] = "";
+        file = new FileReader("src\\codes.txt");
 
-
+        read_file(file);
     }
 }
